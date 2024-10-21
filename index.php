4925ba057612edec3df399ad2649384bac4c04ca
raghveerdeol <deolraghveer5@gmail.com>
2024-10-21 22:47:12 +0200

    added winner funcion

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ if (!isset($_SESSION['boardData']) || count($_SESSION['boardData']) !== 3) {
 }
 $boardData = $_SESSION['boardData'];
 
+// get computer move  
 function game($board) {
     for ($a=0; $a < count($board); $a++) { 
         for ($i=0; $i < count($board[$a]); $i++) { 
@@ -22,8 +23,31 @@ function game($board) {
     return null;
 }
 
+// check the winner 
+function checkWinner($board) {
+    for($i = 0; $i < 3; $i++) {
+        // check row 
+        if ($board[$i][0] === $board[$i][1] && $board[$i][1] === $board[$i][2] && $board[$i][0] !== '') {
+            return 'win';
+        }
+        // check col 
+        if ($board[0][$i] === $board[1][$i] && $board[1][$i] === $board[2][$i] && $board[0][$i] !== '') {
+            return 'win';
+        }
+    }
+    if ($board[0][0] === $board[1][1] && $board[1][1] === $board[2][2] && $board[0][0] !== '') {
+        return 'win';
+    }
+    if ($board[0][2] === $board[1][1] && $board[1][1] === $board[2][0] && $board[0][2] !== '') {
+        return 'win';
+    }
+    return '';
+}
+
 
 $game = '';
+$playerWin = '';
+$computerWin = '';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // get player move 
     $playerMove = $_POST['cell'];
@@ -32,6 +56,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Add player move 
     $boardData[$playerArray[0]][$playerArray[1]] = 'x';
 
+    // check if player win 
+    $playerWin = checkWinner($boardData);
+    if ($playerWin === 'win') {
+        $game = 'The Player Win';
+    }
+
     // get computer move 
     $computerMove = game($boardData);
     if ($computerMove != null) {
@@ -39,12 +69,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $computerArray = str_split($computerMove);
         // ad computer move 
         $boardData[$computerArray[0]][$computerArray[1]] = 'o';
-    }
-    $_SESSION['boardData'] = $boardData;
-    if ($computerMove === null) {
-        $game = 'Game over';
+        // check if computer win 
+        $computerWin = checkWinner($boardData);
+        if ($computerWin === 'win' && $playerWin === '') {
+            $game = 'The Computer Win';
+        }
     }
 
+
+    $_SESSION['boardData'] = $boardData;
+    // if there is no more moves
+    if ($computerMove === null && $playerWin === '' && $computerWin === '') {
+        $game = 'Game over';
+    }
 
 }
 
@@ -65,29 +102,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <main>
         <h1>Play the game</h1>
         <!-- reset game  -->
-        <form action="index.php" method="GET">
-            <button type="submit" name="reset">Start New Game</button>
+        <form action="index.php" method="GET" id="newGameForm">
+            <button type="submit" name="reset" class="newGame">Start New Game</button>
         </form>
         <!-- game board  -->
         <form action="index.php" method="POST" id="gameForm" class="gameContainer">
             <?php for ($a=0; $a < count($boardData); $a++) { ?>
                 <?php for ($i=0; $i < count($boardData[$a]); $i++) { ?>
                     <input 
-                    type="radio" 
-                class="square 
-                <?php if ($boardData[$a][$i] === 'x') { ?>
-                    tic
-                <?php } if ($boardData[$a][$i] === 'o') { ?>
-                    toe
-                <?php } ?> " 
-                    name="cell" 
-                    value="<?php echo $a.$i ?>"
-                    <?php if ($boardData[$a][$i] !== '') { ?> disabled <?php } ?>
-                    >
-                </input>
+                        type="radio" 
+                        class="square 
+                        <?php if ($boardData[$a][$i] === 'x') { ?>
+                            tic
+                        <?php } if ($boardData[$a][$i] === 'o') { ?>
+                            toe
+                        <?php } ?> " 
+                        name="cell" 
+                        value="<?php echo $a.$i ?>"
+                        <?php if ($boardData[$a][$i] !== '' || $game !== '') { ?> disabled <?php } ?>
+                        >
                 <?php } ?>
             <?php } ?>
-            <button type="submit">Submit your move</button>
+            <button type="submit"  class="newGame">Submit your move</button>
         </form>
         <section>
             <h2>
